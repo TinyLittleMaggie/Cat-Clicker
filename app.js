16f32ac8340c6e8ae336ad949d02e8e002102bbd
Maggie Gong <meijic@outlook.com>
2019-08-22 17:02:31 +0800

    Scale to two cats

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-var count = 0;
+// Initialise variables
+var count1 = 0;
+var count2 = 0;
+var catName1 = 'Alice';
+var catName2 = 'Jay';
 
-$('.cat').click(function() {
-  count++;
-  $('.num-of-clicks').text('Clicks: ' + count);
+
+// Display cat names
+$('#cat-name-1').text(catName1);
+$('#cat-name-2').text(catName2);
+
+
+// Handle clicks
+$('#cat1').click(function() {
+  count1++;
+  $('#clicks-1').text('Clicks: ' + count1);
+});
+
+$('#cat2').click(function() {
+  count2++;
+  $('#clicks-2').text('Clicks: ' + count2);
 });
