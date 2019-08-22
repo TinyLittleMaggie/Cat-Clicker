03950fde3696c752afbd35537bc0b94fe1ce5608
Maggie Gong <meijic@outlook.com>
2019-08-22 17:56:46 +0800

    Scaled to three cats

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Initialise variables
 var count1 = 0;
 var count2 = 0;
+var count3 = 0;
 var catName1 = 'Alice';
 var catName2 = 'Jay';
+var catName3 = 'Bonnie';
 
 
 // Display cat names
 $('#cat-name-1').text(catName1);
 $('#cat-name-2').text(catName2);
+$('#cat-name-3').text(catName3);
 
 
 // Handle clicks
@@ -19,4 +22,9 @@ $('#cat1').click(function() {
 $('#cat2').click(function() {
   count2++;
   $('#clicks-2').text('Clicks: ' + count2);
+});
+
+$('#cat3').click(function() {
+  count3++;
+  $('#clicks-3').text('Clicks: ' + count3);
 });
